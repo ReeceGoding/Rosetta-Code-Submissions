7836b25d7c35a1e6bdc49837bae5e81572293f27
RG <reece.goding@outlook.com>
2021-10-03 19:42:09 +0100

    Overhaul.

--- R/Factors of an integer.R
+++ R/Factors of an integer.R
@@ -1,13 +1,8 @@
 #https://rosettacode.org/wiki/Factors_of_an_integer
-#Context: The site's other solution uses a list. I considered this unidiomatic.
+#Context: Below. I considered the previous solution unidiomatic.
 
 ##Filter solution
-#With identical output, a more idiomatic way is to use R's Filter and Vectorize: 
-factors<-function(n)
-{
-  allButN<-Filter(function(x) n %% x == 0, 1:(n%/%2))
-  union(allButN,n)#We use union rather than the c() to prevent factors(1) from returning 1 twice.
-}
-
-#If you want to use a vector of integers as an input, as in the previous solution, use:
-manyFactors<-function(vec){Vectorize(factors)(vec)}
+#With identical output, a more idiomatic way is to use R's Filter. 
+factors <- function(n) c(Filter(function(x) n %% x == 0, seq_len(n %/% 2)), n)
+#Vectorize is an interesting alternative to the previous solution's lapply.
+manyFactors <- function(vec) Vectorize(factors)(vec)
