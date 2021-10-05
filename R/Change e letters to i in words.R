2bd33ee271624b97a037ec7db9d313d04cc0fb27
RG <reece.goding@outlook.com>
2021-10-05 22:37:23 +0100

    Two quick and new submissions.

---
+++ R/Change e letters to i in words.R
@@ -0,0 +1,6 @@
+#https://rosettacode.org/wiki/Change_e_letters_to_i_in_words
+#Context: None needed. This is very short. There was no solution in R before mine.
+
+dict <- scan("https://web.archive.org/web/20180611003215/http://www.puzzlers.org/pub/wordlists/unixdict.txt", what = character())
+changed <- chartr("e", "i", dict)
+cbind(old = dict, new = changed)[changed != dict & changed %in% dict & nchar(changed) > 5, ]
