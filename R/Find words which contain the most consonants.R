1f2f23813c5cacd706bfca819e9bfe3eac2df9e7
RG <reece.goding@outlook.com>
2021-10-09 17:42:46 +0100

    Variable name change.

--- R/Find words which contain the most consonants.R
+++ R/Find words which contain the most consonants.R
@@ -9,9 +9,9 @@ consonants <- letters[!letters %in% c("a", "e", "i", "o", "u")]
 #The following line is equivalent to sapply(consonants, function(x) stringr::str_count(dictBig, x))
 #As with all things with strings in R, life is easier with stringr or stringi.
 consonantCount <- sapply(consonants, function(x) lengths(regmatches(dictBig, gregexec(x, dictBig))))
-singleConstWords <- dictBig[apply(consonantCount, MARGIN = 1, function(x) all(x <= 1))]
-uniqueConstCount <- rowSums(sapply(consonants, function(x) grepl(x, singleConstWords)))
-biggestWords <- function(offset) singleConstWords[which(uniqueConstCount == max(uniqueConstCount) - offset)]
+wordsWithNoRepeatedConsts <- dictBig[apply(consonantCount, MARGIN = 1, function(x) all(x <= 1))]
+uniqueConstCount <- rowSums(sapply(consonants, function(x) grepl(x, wordsWithNoRepeatedConsts)))
+biggestWords <- function(offset) wordsWithNoRepeatedConsts[which(uniqueConstCount == max(uniqueConstCount) - offset)]
 cat("Biggest:", paste(biggestWords(0), collapse = ", "),
     "\n \n Second biggest:", paste(biggestWords(1), collapse = ", "),
     "\n \n Third biggest:", paste(biggestWords(2), collapse = ", "))
