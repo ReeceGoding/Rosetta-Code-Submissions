715395e593b9ec32912c16a62af6a157345afebb
RG <reece.goding@outlook.com>
2021-10-10 12:04:14 +0100

    Variable removal.

--- R/Find words which contains all the vowels.R
+++ R/Find words which contains all the vowels.R
@@ -4,8 +4,7 @@
 #Adapting this from https://rosettacode.org/wiki/Find_words_which_contain_the_most_consonants#R is trivial.
 dict <- scan("https://web.archive.org/web/20180611003215/http://www.puzzlers.org/pub/wordlists/unixdict.txt", what = character())
 dictBig <- dict[nchar(dict) > 10]
-vowels <- letters[letters %in% c("a", "e", "i", "o", "u")]
-#The following line is equivalent to sapply(vowels, function(x) stringr::str_count(dictBig, x))
+#The following line is equivalent to sapply(c("a", "e", "i", "o", "u"), function(x) stringr::str_count(dictBig, x))
 #As with all things with strings in R, life is easier with stringr or stringi.
-vowelCount <- sapply(vowels, function(x) lengths(regmatches(dictBig, gregexec(x, dictBig))))
+vowelCount <- sapply(c("a", "e", "i", "o", "u"), function(x) lengths(regmatches(dictBig, gregexec(x, dictBig))))
 dictBig[apply(vowelCount, MARGIN = 1, function(x) all(x == 1))]
