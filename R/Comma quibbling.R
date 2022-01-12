63325fa428794f9e3f3965a84880870012a5ad30
RG <reece.goding@outlook.com>
2022-01-12 20:30:39 +0000

    Overhaul.

--- R/Comma quibbling.R
+++ R/Comma quibbling.R
@@ -5,14 +5,10 @@ quib <- function(vect)
 {
   #The task does not consider empty strings to be words, so we remove them immediately.
   #We could also remove non-upper-case characters, but the tasks gives off the impression that the user will do that.
-  vect <- vect[!nchar(vect) == 0]
+  vect <- vect[nchar(vect) != 0]
   len <- length(vect)
-  if(len == 0) return("{}")
-  result <- switch(len,
-                   paste0("{", vect, "}"),
-                   paste0("{", vect[1], " and ", vect[2], "}"))
-  #R's switch for numerics doesn't support default cases, so this next line cannot go in the switch.
-  if(is.null(result)) paste0("{", paste0(vect[seq_len(len-1)], collapse = ", "), " and ", vect[len], "}") else result
+  allButLastWord <- if(len >= 2) paste0(vect[seq_len(len - 1)], collapse = ", ") else ""
+  paste0("{", if(nchar(allButLastWord) == 0) vect else paste0(allButLastWord, " and ", vect[len]), "}")
 }
 quib(character(0)) #R has several types of empty string, e.g. character(0), "", and c("", "", "").
 quib("")
